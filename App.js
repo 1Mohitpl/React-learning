e22d15d86e47557193e6dd029dafd3b6e282b135
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-11 20:02:54 +0530

    git commit

---
+++ App.js
@@ -0,0 +1,14 @@
+const Heading1 = React.createElement("h1", {
+    id:"title1"
+}, "Heading1");
+
+const heading2 = React.createElement("h2",{
+  id:"title2"
+}, "heading2");
+
+const container = React.createElement("div",{
+id:"conatiner"
+}, [Heading1, heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
