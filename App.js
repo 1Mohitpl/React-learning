2e2c250636ccab1efc037ef4e704ec052155ed3b
Mohit kumar paul <mohit1paul@gmail.com>
2024-06-21 00:49:42 +0530

    add button in my react

--- App.js
+++ App.js
@@ -2,20 +2,34 @@ import React from "react";
 import ReactDOM from "react-dom/client";
     // writing code in react in class functional way 
 const heading = (
-<h1 id="titele" key="h2">                         
+<h1  id="titele" key="h2">                         
 i want be an good software enginner
 </h1>);
 
 
 // writing react code in functional component
-const heading1 = ()=>{
-    return ( 
+const Heading1 = function(){       
+    return (  
     <div><h1>nameste react</h1> </div>);
 };
 
-const heading2 = ()=>{    // just like arrow function
-  return <div><h1>Hello raect is just beast</h1></div>
-}
+const HeadingComponent = ()=>{    // just like arrow function  // compoenent composition
+  return( 
+  
+<div className="main">   
+<div>
+
+     
+     <button type="clik">person</button>
+</div>                             
+    <Heading1 />    
+     {heading}
+  <h2>Hello raect is just beast </h2> 
+  <h2> hey,i am fronted enginner</h2>
+  
+</div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading1);
+root.render(<HeadingComponent  />);
