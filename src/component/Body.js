32c5222a41cf5fc2b7aa1d06834718a1ea4a96a2
Mohit kumar paul <mohit1paul@gmail.com>
2024-08-04 19:38:55 +0530

    build search functionality

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,13 +1,34 @@
 import {RestrauCrad} from "../constant";
+import {useState} from "react";
 
 
 const Body = () => {
+  // const searchtxt = "KFC"; // this is how created varibale in normla javascript it is hardcoded value, 
+  // searchtext is a local state variable
+  const [searchtext, setSearchText] = useState("KFC");  // this is way  to create state variables comes form react
+
+  const [searchClicked, setsearchCliked] = useState("False");
+
     return (
         <>
     <div className="search-Box">
-      <input type="text" className="search-restaurent" placeholder="Search" value=""/>
-         
+      <input type="text" 
+      className="search-restaurent" 
+      placeholder="Search" 
+      value={searchtext}
+      onChange={(e)=>{
+        setSearchText( e.target.value);
+      }}
+      />
+
+     <h1>{searchClicked}</h1>
+      <button className ="search-btn" 
+      onClick={(e)=>{
+      setsearchCliked("True")}}
+      >Search</button>
+
     </div>
+
       <div className="Restrau-list">
       <>
       <RestrauCrad />
